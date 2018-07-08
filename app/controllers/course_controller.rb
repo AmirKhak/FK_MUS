29a837f217e9ac526c2316ea7ff6c15384689c07
ak8815 <ak8815@ic.ac.uk>
2018-07-08 20:36:04 +0100

    Implemented Courses Controller.

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -1,2 +1,52 @@
 class CourseController < ApplicationController
+  def index
+    @course = Course.all;
+    @admin = isAdmin;
+  end
+
+  def create
+    @course = Course.new(course_params)
+
+    if @course.save
+      flash[:notice] = "با موفقیت اضافه شد"
+    else
+      flash[:alert] = "درخواست شما با خطا مواجه شد"
+    end
+    redirect_to course_index_path
+  end
+
+  def update
+    @course = Course.find(params[:id])
+
+    if @course.update(course_params)
+      flash[:notice] = "تغییر با موفقیت انجام شد"
+    else
+      flash[:alert] = "درخواست شما با خطا مواجه شد"
+    end
+    redirect_to course_index_path
+  end
+
+  def destroy
+    if isAdmin
+      course = Course.find(params[:id])
+      course.destroy
+    end
+
+    redirect_to course_index_path
+  end
+
+  private
+
+  def isAdmin
+    if user_signed_in?
+      if current_user.admin
+        return true;
+      end
+    end
+    return false;
+  end
+
+  def course_params
+    params.require(:course).permit(:answer, :question)
+  end
 end
